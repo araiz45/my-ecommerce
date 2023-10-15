dc98efcd214db6bd46a4a0b1b0c2af2e6bdc6623
Aaraiz Baig <araizzahid45@gmail.com>
2023-10-16 02:32:17 +0500

    done

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
 export const instance = axios.create({
-  baseURL: 'http://localhost:1337',
+  baseURL: 'https://injir-2dda26730101.herokuapp.com',
   headers: {
     "Content-Type": "application/json",
-    "Authorization": "Bearer 56384a11051648ed6fb8615a2100395cf1140b5c72cd24f5f9cd48a30b22090e69eacde21684ba0c0c4486e4e61b9a358ea372e27ae2ea4b7a90eec4bef5b6cca0b841afb8b7abf3ffdee8fbbdc1494543226ee0daf9534b6a69b1a1071c1478d7c1230b431b1ed807b406a6bf5e88e6142e7bfad972328744a47e5eb4c4e765"
+    "Authorization": "Bearer c314766950963203b6187da7e4db7a571b2e8155005e4f01b10d3c05a32306538ebcb480f2935066199481fc6abcfbc716080e9bff41fa6c9fdfcc6afd5ce922714cb1150748c8a5db8f6030c5fed9f8e5d9e79b5c921cfbbc5b101cac676f78483fb335f1b1052c52a544e270712ee9dd9cffd13aeb979c78f195576e840328"
   }
 })
 
-export const BASEURL = 'http://localhost:1337'
+export const BASEURL = 'https://injir-2dda26730101.herokuapp.com'
